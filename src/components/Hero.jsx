2d853cafed65c083fc9da132947afb6975fc0626
Joshua Gichuhi <164097287+Njugunaaa@users.noreply.github.com>
2025-09-23 09:35:51 +0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 
 const heroImages = [
-  "src/assets/images/1.jpg",
+  "src/assets/images/7.jpg",
+  "src/assets/images/5.jpg",
   "src/assets/images/2.jpg",
-  "src/assets/images/3.jpg",
 ];
 
 const heroContent = [
